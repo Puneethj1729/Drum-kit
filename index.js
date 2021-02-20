07bd0d1703350249873871273c68ffc3f8a72810
Puneethj1729 <60792837+Puneethj1729@users.noreply.github.com>
2021-02-21 00:41:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+function playMusic(value) {
+  switch (value) {
+    case "w":
+      var audio1 = new Audio('sounds/crash.mp3');
+      audio1.play();
+      break;
+    case "a":
+      var audio2 = new Audio('sounds/kick-bass.mp3');
+      audio2.play();
+      break;
+    case "s":
+      var audio3 = new Audio('sounds/snare.mp3');
+      audio3.play();
+      break;
+    case "d":
+      var audio4 = new Audio('sounds/tom-1.mp3');
+      audio4.play();
+      break;
+    case "j":
+      var audio5 = new Audio('sounds/tom-2.mp3');
+      audio5.play();
+      break;
+    case "k":
+      var audio6 = new Audio('sounds/tom-3.mp3');
+      audio6.play();
+      break;
+    case "l":
+      var audio7 = new Audio('sounds/tom-4.mp3');
+      audio7.play();
+      break;
+    default:
+      console.log('error');
+
+  }
+}
+
+
+for (var i = 0; i < document.querySelectorAll('.drum').length; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener("click",function(){
+    var key=this.innerHTML;
+    playMusic(key);
+    animateButton(key);
+  });
+}
+document.addEventListener('keydown',function(event){
+  playMusic(event.key);
+  animateButton(event.key);
+});
+function animateButton(key){
+   var pressedButton=document.querySelector('.'+key);
+   pressedButton.classList.add('pressed');
+   setTimeout(function(){
+     pressedButton.classList.remove('pressed');
+   },500);
+}
